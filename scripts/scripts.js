8d3946c46de7a97ec174d110f116544fe520c915
goncalves-ac <goncalves.ac@outlook.com>
2020-09-29 18:36:52 -0300

    Add SVG's de algumas imagens

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,24 +2,27 @@ console.log('Aparece mensagem')
 
 /* Mudar a cor do modal */
 
-var GetColor = document.querySelector('#getColor');
+window.addEventListener('load', function() {
+    var GetColor = document.querySelector('#getColor');
 
-var Change = document.querySelector('#changeColor');
-var ChangeBtn = document.querySelector('#changeColorBtn');
-var ChangeBackground = document.querySelector('#changeColorBackground');
-var ChangeColorBoxShadow = document.querySelector('#changeColorBoxShadow');
+    var Change = document.querySelector('#changeColor');
+    var ChangeBtn = document.querySelector('#changeColorBtn');
+    var ChangeBackground = document.querySelector('#changeColorBackground');
+    var ChangeBoxShadow = document.getElementById('changeColorBoxShadow');
 
-Change.style.backgroundColor = GetColor.value;
-ChangeBtn.style.backgroundColor = GetColor.value;
-ChangeBackground.style.backgroundColor = GetColor.value;
-//ChangeColorBoxShadow.style.boxShadow = `0 0 0 10px ${GetColor.value}`;
+    Change.style.backgroundColor = GetColor.value;
+    ChangeBtn.style.backgroundColor = GetColor.value;
+    ChangeBackground.style.backgroundColor = GetColor.value;
+    ChangeBoxShadow.style.boxShadow = `0px 0px 0px 10px ${GetColor.value}`;
 
-GetColor.addEventListener('input', function(e) {
-    Change.style.backgroundColor = e.target.value;
-    ChangeBtn.style.backgroundColor = e.target.value;
-    ChangeBackground.style.backgroundColor = e.target.value;
-    //ChangeColorBoxShadow.style.boxShadow = `0 0 0 10px ${e.target.value}`;
-}, false);
+    GetColor.addEventListener('input', function(e) {
+        Change.style.backgroundColor = e.target.value;
+        ChangeBtn.style.backgroundColor = e.target.value;
+        ChangeBackground.style.backgroundColor = e.target.value;
+        ChangeBoxShadow.style.boxShadow = `0px 0px 0px 10px ${GetColor.value}`;
+    }, false);
+
+});
 
 /* Fim mudar cor */
 
