3cb84e9724268eb2eb4ddbbf5609055f7d53f550
goncalves-ac <goncalves.ac@outlook.com>
2020-09-29 01:34:03 -0300

    Add js changeColor e modal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,40 @@
 console.log('Aparece mensagem')
+
+/* Mudar a cor do modal */
+
+var GetColor = document.querySelector('#getColor');
+
+var Change = document.querySelector('#changeColor');
+var ChangeBtn = document.querySelector('#changeColorBtn');
+var ChangeBackground = document.querySelector('#changeColorBackground');
+var ChangeColorBoxShadow = document.querySelector('#changeColorBoxShadow');
+
+Change.style.backgroundColor = GetColor.value;
+ChangeBtn.style.backgroundColor = GetColor.value;
+ChangeBackground.style.backgroundColor = GetColor.value;
+//ChangeColorBoxShadow.style.boxShadow = `0 0 0 10px ${GetColor.value}`;
+
+GetColor.addEventListener('input', function(e) {
+    Change.style.backgroundColor = e.target.value;
+    ChangeBtn.style.backgroundColor = e.target.value;
+    ChangeBackground.style.backgroundColor = e.target.value;
+    //ChangeColorBoxShadow.style.boxShadow = `0 0 0 10px ${e.target.value}`;
+}, false);
+
+/* Fim mudar cor */
+
+function iniciaModal(modalID){
+    const modal = document.getElementById(modalID);
+    if (modal) {
+        modal.classList.add('open');
+        modal.addEventListener('click', (e) => {
+            if(e.target.id == modalID || e.target.className == 'close'){
+                modal.classList.remove('open');
+            }
+        });
+    }
+}
+
+function abreModal () {
+    iniciaModal('addEvent');
+}
