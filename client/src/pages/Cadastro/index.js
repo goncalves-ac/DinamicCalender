6df404374e04936a9058d834108fb11f36c03f41
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-10-27 07:18:15 -0300

    Modifica padding de alguns inputs, adiciona "hack" no input de data de nascimento para mostrar placeholder

--- client/src/pages/Cadastro/index.js
+++ client/src/pages/Cadastro/index.js
@@ -3,6 +3,16 @@ import Logo_Black from "./../../img/logo-black.png";
 import "./style.css";
 
 export default function Cadastro() {
+    const birthInput = React.createRef();
+
+    const handleBirthInputFocus = () => birthInput.current.type="date";
+    const handleBirthInputBlur = () => {
+        if (birthInput.current.value==="") {
+            birthInput.current.type="text";
+        }
+    }        
+
+
     return (
         <div className="row no-gutters my-bg-orange-0" id="container-base-cadastro">
             <div className="align-self-center container-md text-center col-lg-5">
@@ -11,17 +21,20 @@ export default function Cadastro() {
                     <h2 className="h3 mb-3 font-weight-normal">Cadastro</h2>
 
                     <div className="row no-gutters">
-                        <input name="nome" type="email" className="col-md-6 col-sm-12 form-control p-1" placeholder="Nome" required=""/>
-                        <input name="sobrenome" type="email" className="col-md-6 col-sm-12 form-control p-1" placeholder="Sobrenome" required=""/>
+                        <input name="nome" type="text" className="col-md-6 col-sm-12 form-control p-2" placeholder="Nome" required/>
+                        <input name="sobrenome" type="text" className="col-md-6 col-sm-12 form-control p-2" placeholder="Sobrenome" required/>
                     </div>
 
-                    <input name="email" type="email" className="form-control my-1" placeholder="Email" required=""/>
-                    <input name="senha" type="password" className="form-control my-1" placeholder="Senha" required=""/>
+                    <input name="email" type="email" className="form-control my-1 p-2" placeholder="Email" required/>
+                    <input name="senha" type="password" className="form-control my-1 p-2" placeholder="Senha" required/>
 
                     <div className="row no-gutters">
-                        <input type="date" className="col-md-6 col-sm-12 form-control p-1"
-                               placeholder="Data de Nascimento"/>
-                        <select className="col-md-6 col-sm-12 form-control p-1" name="genero" required="">
+                        <input type="text" className="col-md-6 col-sm-12 form-control p-2"
+                               placeholder="Data de Nascimento"
+                               onFocus={handleBirthInputFocus}
+                               onBlur={handleBirthInputBlur}
+                               ref={birthInput} />
+                        <select className="col-md-6 col-sm-12 form-control p-2" name="genero" required>
                             <option disabled="" hidden="" selected="selected">Selecione seu Gênero</option>
                             <option value="m">Masculino</option>
                             <option value="f">Feminino</option>
