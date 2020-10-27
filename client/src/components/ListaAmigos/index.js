3af9b2754c031675d1736f5b7b5e490b41fb7022
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-10-27 07:17:11 -0300

    Refatora ListaAmigos para utilizar componente CardAmigo

--- client/src/components/ListaAmigos/index.js
+++ client/src/components/ListaAmigos/index.js
@@ -1,7 +1,36 @@
 import React from "react";
+import CardAmigo from "../CardAmigo";
 import "./ListaAmigos.css";
 
 const ListaAmigos = () => {
+    const amigos = [
+        {
+        avatarUrl:"https://bootdey.com/img/Content/avatar/avatar7.png",
+        userName:"Sophia Silva",
+        profileLink:"#",
+        userDesc:"Assim como tudo na vida, todo início de um processo deve ser realizado com cautela."
+    },
+    {
+        avatarUrl:"https://bootdey.com/img/Content/avatar/avatar6.png",
+        userName:"Lucio Santos",
+        profileLink:"#",
+        userDesc:"Aprenda a viver sem as pessoas que vivem sem você."
+    },
+    {
+        avatarUrl:"https://bootdey.com/img/Content/avatar/avatar5.png",
+        userName:"Ariel Pereira",
+        profileLink:"#",
+        userDesc:"Em um mundo feito de aparências, feliz é aquele que é feito de verdades."
+    },
+    {
+        avatarUrl:"https://bootdey.com/img/Content/avatar/avatar3.png",
+        userName:"Ana Carol",
+        profileLink:"#",
+        userDesc:"Um brinde ao que é vivido e não postado."
+    },
+]
+
+
     return (
         <div className=" col-lg-6 offset-lg-3 col-md-10 offset-md-1 col-sm-12">
 
@@ -15,73 +44,7 @@ const ListaAmigos = () => {
             </form>
 
             <div className="people-nearby">
-                <div className="nearby-user">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-3 col-sm-9">
-                            <img alt="user" className="profile-photo-lg"
-                                 src="https://bootdey.com/img/Content/avatar/avatar7.png" />
-                        </div>
-                        <div className="col-lg-7 col-md-7 col-sm-12">
-                            <h5><a className="profile-link" href="#">Sophia Silva</a></h5>
-                            <p>Assim como tudo na vida, todo início de um processo deve ser realizado com cautela.</p>
-                        </div>
-                        <div className="col-lg-2 col-md-2 col-sm-3">
-                            <button className="btn btn-info btn-circle pull-right"><i
-                                className="fas fa-user-plus fa-lg"></i></button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="nearby-user">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-3 col-sm-9">
-                            <img alt="user" className="profile-photo-lg"
-                                 src="https://bootdey.com/img/Content/avatar/avatar6.png" />
-                        </div>
-                        <div className="col-lg-7 col-md-7 col-sm-12">
-                            <h5><a className="profile-link" href="#">Lucio Santos</a></h5>
-                            <p>Aprenda a viver sem as pessoas que vivem sem você.</p>
-                        </div>
-                        <div className="col-lg-2 col-md-2 col-sm-3">
-                            <button className="btn btn-info btn-circle pull-right"><i
-                                className="fas fa-user-plus fa-lg"></i></button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="nearby-user">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-3 col-sm-9">
-                            <img alt="user" className="profile-photo-lg"
-                                 src="https://bootdey.com/img/Content/avatar/avatar5.png" />
-                        </div>
-                        <div className="col-lg-7 col-md-7 col-sm-12">
-                            <h5><a className="profile-link" href="#">Ariel Pereira</a></h5>
-                            <p>Em um mundo feito de aparências, feliz é aquele que é feito de verdades.</p>
-                        </div>
-                        <div className="col-lg-2 col-md-2 col-sm-3">
-                            <button className="btn btn-info btn-circle pull-right"><i
-                                className="fas fa-user-plus fa-lg"></i></button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="nearby-user">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-3 col-sm-9">
-                            <img alt="user" className="profile-photo-lg"
-                                 src="https://bootdey.com/img/Content/avatar/avatar3.png" />
-                        </div>
-                        <div className="col-lg-7 col-md-7 col-sm-12">
-                            <h5><a className="profile-link" href="#">Ana Carol</a></h5>
-                            <p>Um brinde ao que é vivido e não postado.</p>
-                        </div>
-                        <div className="col-lg-2 col-md-2 col-sm-3">
-                            <button className="btn btn-info btn-circle pull-right"><i
-                                className="fas fa-user-plus fa-lg"></i></button>
-                        </div>
-                    </div>
-                </div>
+                {amigos.map(amigo => <CardAmigo userInfo={amigo}/>)}
             </div>
         </div>
     );
