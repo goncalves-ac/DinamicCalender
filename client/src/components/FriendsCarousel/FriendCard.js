1e5c79c8a50fb7de335ddfe10b0eeee863360909
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-03 21:26:58 -0300

    Componente carrossel de amigos

---
+++ client/src/components/FriendsCarousel/FriendCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const FriendCard = ({ friend }) => {
+  return (
+    <div className="carousel-friend-card">
+      <img
+        className="carousel-card-avatar"
+        src={friend.avatarUrl}
+        alt={friend.name}
+      />
+      <h3 className="carousel-card-user-name">{friend.name}</h3>
+      <Link className="carousel-check-profile-btn" to={friend.profileUrl}>
+        Ver Perfil
+      </Link>
+    </div>
+  );
+};
+
+export default FriendCard;
---
+++ client/src/components/FriendsCarousel/carouselSettings.js
@@ -0,0 +1,9 @@
+export default {
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  centerMode: true,
+  centerPadding: "3px",
+};
