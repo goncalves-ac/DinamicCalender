aeca87b4ee4ed615df7fed195e565a77f8c8e57e
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-11 08:47:09 -0300

    Atualizações nos repositories das entidades

--- api/src/main/java/com/example/demo/model/repositories/EventoRepository.java
+++ api/src/main/java/com/example/demo/model/repositories/EventoRepository.java
@@ -1,9 +1,21 @@
 package com.example.demo.model.repositories;
 
 import com.example.demo.model.entities.Evento;
-import org.springframework.data.repository.CrudRepository;
 
-public interface EventoRepository extends CrudRepository<Evento, Integer> {
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface EventoRepository extends CrudRepository<Evento, UUID> {
     // Metodos
     Evento findByTitulo(String titulo);
+    
+    Evento findByInicio(Date data);
+
+    Set<Evento> findByFkIdDono(@Param("id") UUID id);
+
 }
