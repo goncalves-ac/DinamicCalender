d24fe5a82d2754dd7f1754d33b48ea183a56d83a
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-11 08:45:28 -0300

    Atualiza entidade amigo (auto relacionamento de usuário)

--- api/src/main/java/com/example/demo/model/entities/Amigo.java
+++ api/src/main/java/com/example/demo/model/entities/Amigo.java
@@ -1,42 +1,26 @@
 package com.example.demo.model.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Table(name="amigo")
+@Getter
+@Setter
 public class Amigo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_amizade;
 
-    @ManyToOne
-    @JoinColumn(name = "fk_id_usuario")
-    @JsonIgnoreProperties("amigos")
-    private Usuario usuario_1;
-    private Usuario usuario_2;
+    private int id_usuario_1;
+
+    private int id_usuario_2;
+    
     private int status;
 
     public Amigo() { }
 
-    public int getId_amizade() {
-        return id_amizade;
-    }
-
-    public Usuario getUsuario_1() {
-        return usuario_1;
-    }
-
-    public Usuario getUsuario_2() {
-        return usuario_2;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
 }
