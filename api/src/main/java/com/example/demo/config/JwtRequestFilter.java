e8a9f583386c5bd9583083769c97d77c05a51a06
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-12 05:12:55 -0300

    Formatação das importações

--- api/src/main/java/com/example/demo/config/JwtRequestFilter.java
+++ api/src/main/java/com/example/demo/config/JwtRequestFilter.java
@@ -13,7 +13,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import com.example.demo.service.JwtUserDetailsService;
+
+import com.example.demo.services.JwtUserDetailsService;
+
 import io.jsonwebtoken.ExpiredJwtException;
 
 @Component
