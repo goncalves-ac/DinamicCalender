2b2915772cc1ee70612ae7c0306a64f32e7b2c03
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-12 05:15:44 -0300

    DTO de requisição e resposta do recurso usuário

---
+++ api/src/main/java/com/example/demo/dto/UsuarioResponseDTO.java
@@ -0,0 +1,46 @@
+package com.example.demo.dto;
+
+import java.util.Date;
+import java.util.Set;
+
+import com.example.demo.model.entities.Evento;
+import com.example.demo.model.entities.Usuario;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+
+@Getter
+public class UsuarioResponseDTO {
+	private int idUsuario;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private Date nascimento;
+	private String genero;
+	
+	@JsonIgnoreProperties(value={"eventosAlheios", "senha", "email", "genero", "amigosRequisitados"
+			, "requisicoesAmigos", "eventosProprios"})
+	private Set<Usuario> amigosRequisitados;
+    
+	@JsonIgnoreProperties(value={"eventosAlheios", "senha", "email", "genero", "amigosRequisitados"
+			, "requisicoesAmigos", "eventosProprios"})
+    private Set<Usuario> requisicoesAmigos;
+    
+    private Set<Evento> eventosAlheios;
+
+    private Set<Evento> eventosProprios;
+	
+	public UsuarioResponseDTO(Usuario usuario) {
+		this.idUsuario = usuario.getIdUsuario();
+		this.nome=usuario.getNome();
+		this.sobrenome=usuario.getSobrenome();
+		this.email=usuario.getEmail();
+		this.nascimento=usuario.getNascimento();
+		this.genero=usuario.getGenero();
+		this.amigosRequisitados = usuario.getAmigosRequisitados();
+		this.requisicoesAmigos = usuario.getRequisicoesAmigos();
+		this.eventosAlheios = usuario.getEventosAlheios();
+		this.eventosProprios = usuario.getEventosProprios();
+	}
+	
+}
