ba02eda21d47bedf2a232a2b67b653888c88f752
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-12 05:13:17 -0300

    ID de usuário redefinido como int

--- api/src/main/java/com/example/demo/config/UserDetail.java
+++ api/src/main/java/com/example/demo/config/UserDetail.java
@@ -1,12 +1,9 @@
 package com.example.demo.config;
 
 import java.util.Collection;
-import java.util.UUID;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-
-
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,9 +13,9 @@ import lombok.Setter;
 public class UserDetail extends User{
 
 	private static final long serialVersionUID = -127653421684538888L;
-	private UUID idUsuario;
+	private int idUsuario;
 
-	public UserDetail(UUID id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+	public UserDetail(int id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
 		super(username, password, authorities);
 		this.idUsuario = id;
 	}
