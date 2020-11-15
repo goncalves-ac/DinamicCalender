135345b4874005513cdc843539164c27252ea763
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-15 18:26:56 -0300

    Adiciona userInfo e expiresAt a resposta da autenticação

--- api/src/main/java/com/example/demo/model/entities/JwtResponse.java
+++ api/src/main/java/com/example/demo/model/entities/JwtResponse.java
@@ -1,6 +1,9 @@
 
 package com.example.demo.model.entities;
 import java.io.Serializable;
+import java.util.Date;
+
+import com.example.demo.dto.UsuarioResponseDTO;
 
 import lombok.Getter;
 
@@ -9,8 +12,12 @@ public class JwtResponse implements Serializable {
 
 private static final long serialVersionUID = -8091879091924046844L;
 private final String jwttoken;
+private final UsuarioResponseDTO infoUsuario;
+private final Date expiresAt;
 
-public JwtResponse(String jwttoken) {
+public JwtResponse(String jwttoken, UsuarioResponseDTO infoUsuario, Date expiresAt) {
 	this.jwttoken = jwttoken;
+	this.infoUsuario = infoUsuario;
+	this.expiresAt = expiresAt;
 }
 }
