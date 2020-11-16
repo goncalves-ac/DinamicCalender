c3873494975eb0bb4813e0c9e3e245cb97fa1d95
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-16 02:02:23 -0300

    AuthProvider usando Context API

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import Routes from './Routes';
+import React from "react";
+import { AuthProvider } from "./providers/AuthProvider";
+import Routes from "./Routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
+  );
 }
 
 export default App;
