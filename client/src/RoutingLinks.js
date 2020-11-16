7a7159392764621e7c2e3b1c34f04c3baf9e1e54
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-16 11:37:41 -0300

    Formatação do código com prettier

--- client/src/RoutingLinks.js
+++ client/src/RoutingLinks.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const RoutingLinks = () => {
   return (
     <div className="container d-flex flex-column pt-3">
       <h2 className="text-white">ENDPOINTS</h2>
-      <Link className="mt-2" to="/login">/login</Link>
-      <Link className="mt-2" to="/cadastro">/cadastro</Link>
-      <Link className="mt-2" to="/usuario">/usuario</Link>
-      <Link className="mt-2" to="/calendario">/calendario</Link>
+      <Link className="mt-2" to="/login">
+        /login
+      </Link>
+      <Link className="mt-2" to="/cadastro">
+        /cadastro
+      </Link>
+      <Link className="mt-2" to="/usuario">
+        /usuario
+      </Link>
+      <Link className="mt-2" to="/calendario">
+        /calendario
+      </Link>
     </div>
   );
 };
