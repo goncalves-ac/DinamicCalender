959ff4b89ea5580946dd8ae94894e5127640dd20
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-18 18:24:02 -0300

    Configuração de CORS

--- api/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ api/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,4 +1,6 @@
 package com.example.demo.config;
+import java.util.Arrays;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -51,15 +58,34 @@ public AuthenticationManager authenticationManagerBean() throws Exception {
 return super.authenticationManagerBean();
 }
 
+@Bean
+public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+    StrictHttpFirewall firewall = new StrictHttpFirewall();
+    firewall.setAllowUrlEncodedSlash(true);
+    firewall.setAllowSemicolon(true);
+    return firewall;
+}
+
+@Bean
+public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+    configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+}
 
 @Override
 protected void configure(HttpSecurity httpSecurity) throws Exception {
-httpSecurity.csrf().disable()
+httpSecurity.cors().and().csrf().disable()
 .authorizeRequests().antMatchers(HttpMethod.POST, "/authenticate").permitAll()
 .antMatchers(HttpMethod.POST, "/usuario").permitAll()
 .antMatchers(HttpMethod.GET, "/usuario/**").permitAll()
 .antMatchers(HttpMethod.GET, "/static/**").permitAll()
-.anyRequest().authenticated().and().cors().and().
+.anyRequest().authenticated().and().
 exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
