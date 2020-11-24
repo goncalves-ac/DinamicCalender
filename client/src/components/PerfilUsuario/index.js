78a6013c0018bb1ebac0536ac6589cd7cb2a5f19
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-24 17:28:52 -0300

    Renomeia pasta para ListaEventos

--- client/src/components/PerfilUsuario/index.js
+++ client/src/components/PerfilUsuario/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./PerfilUsuario.css";
-import ListaEventos from "../ListaEvento";
+import ListaEventos from "../ListaEventos";
 
 const PerfilUsuario = () => {
-    return (
-        <div className="d-flex justify-content-around align-self-center">
-            <div className="container mt-3 mb-5">
-                <h3 className="my-blue-1 my-bolder my-text-align"><strong>Eventos</strong></h3>
-                <div className="row">
-                    <ListaEventos />
-                </div>
-            </div>
+  return (
+    <div className="d-flex justify-content-around align-self-center">
+      <div className="container mt-3 mb-5">
+        <h3 className="my-blue-1 my-bolder my-text-align">
+          <strong>Eventos</strong>
+        </h3>
+        <div className="row">
+          <ListaEventos />
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default PerfilUsuario;
