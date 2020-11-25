81ad059a6eeb7e2d56ff58798775ba92c2e4f62e
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-25 02:00:27 -0300

    Integração de eventos ao perfil de usuário

--- client/src/components/ListaEventos/index.js
+++ client/src/components/ListaEventos/index.js
@@ -1,38 +1,18 @@
 import React from "react";
+import Loading from "../../pages/Loading";
 import CardEvento from "../CardEvento";
 import "./ListaEvento.css";
 
-const ListaEventos = () => {
-  const events = [
-    {
-      id: 1,
-      eventTitle: "New Web Design",
-      eventDate: "21 March, 2014",
-      eventDesc:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula...",
-    },
-    {
-      id: 2,
-      eventTitle: "New Web Design",
-      eventDate: "4 March, 2014",
-      eventDesc:
-        "Curabitur purus sem, malesuada eu luctus eget, suscipit sed turpis. Nam pellentesque felis vitae justo accumsan, sed semper nisi sollicitudin...",
-    },
-    {
-      id: 3,
-      eventTitle: "Awesome Employers",
-      eventDate: "1 April, 2014",
-      eventDesc:
-        "Fusce ullamcorper ligula sit amet quam accumsan aliquet. Sed nulla odio, tincidunt vitae nunc vitae, mollis pharetra velit. Sed nec tempor nibh...",
-    },
-  ];
-
+const ListaEventos = ({ nextEvents }) => {
   return (
     <div className="col-md-6 offset-md-3">
       <ul className="timeline">
-        {events.map((event) => (
-          <CardEvento key={event.id} eventInfo={event} />
-        ))}
+        {nextEvents == null && <Loading />}
+        {nextEvents !== null &&
+          nextEvents.length > 0 &&
+          nextEvents.map((event) => (
+            <CardEvento key={event.id_evento} eventInfo={event} />
+          ))}
       </ul>
     </div>
   );
--- client/src/components/PerfilUsuario/index.js
+++ client/src/components/PerfilUsuario/index.js
@@ -2,15 +2,21 @@ import React from "react";
 import "./PerfilUsuario.css";
 import ListaEventos from "../ListaEventos";
 
-const PerfilUsuario = () => {
+const PerfilUsuario = ({ self = false, nextEvents }) => {
   return (
-    <div className="d-flex justify-content-around align-self-center">
+    <div className="d-flex justify-content-around align-self-center full-height">
       <div className="container mt-3 mb-5">
         <h3 className="my-blue-1 my-bolder my-text-align">
-          <strong>Eventos</strong>
+          <strong>Eventos Futuros</strong>
         </h3>
+        {nextEvents !== null && nextEvents.length === 0 && (
+          <p className="text-danger text-center">
+            {(self && "Você") || "Usuário"} não tem nenhum evento público
+            futuro.
+          </p>
+        )}
         <div className="row">
-          <ListaEventos />
+          <ListaEventos nextEvents={nextEvents} />
         </div>
       </div>
     </div>
