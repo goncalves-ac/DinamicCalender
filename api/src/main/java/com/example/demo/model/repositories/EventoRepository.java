718e0df47f4ad1c37355a223c61ce83e740f9873
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-25 19:15:16 -0300

    Reset merge

--- api/src/main/java/com/example/demo/model/repositories/EventoRepository.java
+++ api/src/main/java/com/example/demo/model/repositories/EventoRepository.java
@@ -17,41 +17,41 @@ public interface EventoRepository extends CrudRepository<Evento, Integer> {
 
     Set<Evento> findByFkIdDono(@Param("id") Integer id);
     
-    @Query(value="SELECT * from Evento WHERE fk_id_dono = :id UNION ALL"
+    @Query(value="SELECT * from evento WHERE fk_id_dono = :id UNION ALL"
     		+ "(SELECT DISTINCT e.* FROM "
-    		+ "(Evento e INNER JOIN Usuario_Evento ue ON id_evento = fk_id_evento) "
+    		+ "(evento e INNER JOIN usuario_evento ue ON id_evento = fk_id_evento) "
     		+ "WHERE fk_id_usuario = :id and ue.status='ACCEPTED')"
     		+ " ORDER BY inicio", nativeQuery=true)
     Set<Evento> findAllEventsByUserId(@Param("id") Integer id);
     
-    @Query(value="SELECT * from Evento WHERE fk_id_dono = :id and privacidade='PUBLIC' UNION ALL"
+    @Query(value="SELECT * from evento WHERE fk_id_dono = :id and privacidade='PUBLIC' UNION ALL"
     		+ "(SELECT DISTINCT e.* FROM "
-    		+ "(Evento e INNER JOIN Usuario_Evento ue ON id_evento = fk_id_evento)"
+    		+ "(evento e INNER JOIN usuario_evento ue ON id_evento = fk_id_evento)"
     		+ " WHERE fk_id_usuario = :id"
     		+ " and privacidade='PUBLIC' and ue.status='ACCEPTED')"
     		+ " ORDER BY inicio", nativeQuery=true)
     Set<Evento> findPublicEventsByUserId(@Param("id") Integer id);
     
-    @Query(value="SELECT DISTINCT e.* FROM Evento e WHERE fk_id_dono = :id"
+    @Query(value="SELECT DISTINCT e.* FROM evento e WHERE fk_id_dono = :id"
     		+ " and e.inicio > now() ORDER BY e.inicio LIMIT :limit", nativeQuery=true)
     Set<Evento> findAllNextRecentEventsByOwnerId(@Param("id") Integer id, @Param("limit") Integer limit);
 
-    @Query(value="SELECT DISTINCT e.* FROM Evento e WHERE fk_id_dono = :id"
+    @Query(value="SELECT DISTINCT e.* FROM evento e WHERE fk_id_dono = :id"
     		+ " and privacidade='PUBLIC' and e.inicio > now()"
     		+ " ORDER BY e.inicio LIMIT  :limit", nativeQuery=true)
     Set<Evento> findPublicNextRecentEventsByOwnerId(@Param("id") Integer id, @Param("limit") Integer limit);
     
-    @Query(value="SELECT * from Evento WHERE fk_id_dono = :id UNION ALL"
+    @Query(value="SELECT * from evento WHERE fk_id_dono = :id UNION ALL"
     		+ "(SELECT DISTINCT e.* FROM "
-    		+ "(Evento e INNER JOIN Usuario_Evento ue ON id_evento = fk_id_evento) "
+    		+ "(evento e INNER JOIN usuario_evento ue ON id_evento = fk_id_evento) "
     		+ "WHERE fk_id_usuario = :id"
     		+ " and e.inicio > now() and ue.status='ACCEPTED')"
     		+ " ORDER BY inicio LIMIT :limit", nativeQuery=true)
     Set<Evento> findAllNextRecentEventsByUserId(@Param("id") Integer id, @Param("limit") Integer limit);
     
-    @Query(value="SELECT * from Evento WHERE fk_id_dono = :id and privacidade='PUBLIC' UNION ALL"
+    @Query(value="SELECT * from evento WHERE fk_id_dono = :id and privacidade='PUBLIC' UNION ALL"
     		+ "(SELECT DISTINCT e.* FROM "
-    		+ "(Evento e INNER JOIN Usuario_Evento ue ON id_evento = fk_id_evento) "
+    		+ "(evento e INNER JOIN usuario_evento ue ON id_evento = fk_id_evento) "
     		+ "WHERE fk_id_usuario = :id"
     		+ " and e.privacidade='PUBLIC' and e.inicio > now() and ue.status='ACCEPTED')"
     		+ " ORDER BY inicio LIMIT  :limit", nativeQuery=true)
--- client/src/components/FriendsCarousel/carouselSettings.js
+++
@@ -1,9 +0,0 @@
-export default {
-  infinite: true,
-  slidesToScroll: 1,
-  slidesToShow: 3,
-  dots: false,
-  arrows: false,
-  centerMode: true,
-  centerPadding: "1rem",
-};
