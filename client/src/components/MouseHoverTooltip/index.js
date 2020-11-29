4af36c1fceeb8d30129d2b01438fac9cd322b808
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-29 14:46:01 -0300

    Melhora responsividade do tooltip

--- client/src/components/MouseHoverTooltip/index.js
+++ client/src/components/MouseHoverTooltip/index.js
@@ -9,10 +9,18 @@ const MouseHoverTooltip = ({ children }) => {
     x: -1000,
     y: -1000,
   });
+  const [selfWidth, setSelfWidth] = useState(0);
+  const windowWidth = window.innerWidth;
 
   const handleMouseMove = (event) => {
     setSelfCoordinates({ x: event.clientX, y: event.clientY });
   };
+
+  useEffect(() => {
+    if (overlayRef.current) {
+      setSelfWidth(overlayRef.current.getBoundingClientRect().width);
+    }
+  },[selfCoordinates])
 
   useEffect(() => {
     window.addEventListener("mousemove", handleMouseMove);
@@ -27,8 +35,10 @@ const MouseHoverTooltip = ({ children }) => {
       ref={overlayRef}
       className="hover-tooltip anim-fade-in"
       style={{
-        top: `${selfCoordinates.y + 10}px`,
-        left: `${selfCoordinates.x + 15}px`,
+        top: `${selfCoordinates.y + 20}px`,
+        left: `${selfCoordinates.x - 
+        (selfCoordinates.x + selfWidth > windowWidth ? 
+        (15 + selfCoordinates.x+selfWidth-windowWidth) : 0)}px`,
       }}
     >
       <div className="tooltip-content">{children}</div>
